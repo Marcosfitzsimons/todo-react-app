0741304246b8780c7626f67e57401686e22aa419
marcosfitzsimons <marcosfitzsimons@gmail.com>
2022-09-14 21:41:00 -0300

    Add header states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import MainContent from './components/MainContent'
+import { useState } from 'react'
 
 function App() {
+
+  const [isMoon, setIsMoon] = useState(true);
+  const [bgImg, setBgImg] = useState(true);
+
+  const handleBgImg = () => {
+    setBgImg(prevValue => !prevValue);
+  }
+
+  const handleMoon = () => {
+    setIsMoon((prevValue) => !prevValue)
+  }
+
+
   return (
     <div className="h-screen bg-primary">
-      <Header />
+      <Header isMoon={isMoon} bgImg={bgImg} handleBgImg={handleBgImg} handleMoon={handleMoon} />
 
       <MainContent />
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,39 @@ import React from "react";
 import { useEffect } from "react";
 import { themeChange } from "theme-change";
 
-const Header = () => {
+const Header = ({ isMoon, handleMoon, bgImg, handleBgImg }) => {
   useEffect(() => {
     themeChange(false);
     // 👆 false parameter is required for react project
   }, []);
 
   return (
-    <header className="header h-56">
+    <header className={bgImg ? "header h-56" : "header h-56 active"}>
       <div className="container max-w-4xl mx-auto p-3 h-28 flex justify-between items-center">
-        <h1 className="tracking-widest text-4xl text-white/95 cursor-pointer">
+        <h1 className="tracking-widest text-4xl text-white/95 cursor-pointer font-bold">
           TODO
         </h1>
-        <button
-          data-toggle-theme="dark,default"
-          data-act-class="ACTIVECLASS"
-          className="switch-container aspect-square h-6 cursor-pointer"
-        ></button>
+        {isMoon ? (
+          <button
+            data-set-theme="dark"
+            data-act-class="ACTIVECLASS"
+            className="switch-container aspect-square h-6 cursor-pointer"
+            onClick={() => {
+              handleMoon();
+              handleBgImg();
+            }}
+          ></button>
+        ) : (
+          <button
+            data-set-theme="default"
+            data-act-class="ACTIVECLASS"
+            className="switch-container active aspect-square h-6 cursor-pointer"
+            onClick={() => {
+              handleMoon();
+              handleBgImg();
+            }}
+          ></button>
+        )}
       </div>
     </header>
   );
