632bbcdf6b4ae6131f1fd3c9476a495a0f12d7e6
marcosfitzsimons <marcosfitzsimons@gmail.com>
2022-09-19 21:40:43 -0300

    Add empty state and auto animate

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import NewTodo from "./NewTodo";
 import TodoNav from "./TodoNav";
+import autoAnimate from "@formkit/auto-animate";
+import { useEffect, useRef } from "react";
+import { FaRegSadTear } from "react-icons/fa";
 
 const MainContent = ({
   todoData,
@@ -10,6 +13,14 @@ const MainContent = ({
   itemsActives,
   showItemsActives,
 }) => {
+  const parentRef = useRef(null);
+
+  useEffect(() => {
+    if (parentRef.current) {
+      autoAnimate(parentRef.current);
+    }
+  }, [parentRef]);
+
   return (
     <main className="relative bottom-24 container mx-auto max-w-4xl px-3">
       <section className="h-14 bg-secondary flex justify-between px-3 items-center rounded-xl text-neutral">
@@ -17,7 +28,7 @@ const MainContent = ({
       </section>
 
       <section className="todo-list-container">
-        <ul className="todo-list p-0 mt-7">
+        <ul className="todo-list p-0 mt-7" ref={parentRef}>
           {todoData.map((todoItem) => {
             return (
               <TodoItem
@@ -28,6 +39,12 @@ const MainContent = ({
               />
             );
           })}
+          {todoData.length === 0 && (
+            <p className="flex flex-col items-center justify-center gap-6 text-3xl my-10 pt-8 text-neutral">
+              <FaRegSadTear className="text-8xl" />
+              You don't have anything to do...
+            </p>
+          )}
         </ul>
       </section>
 
