724f33282c333323db7507351a887cb63f6591a2
marcosfitzsimons <marcosfitzsimons@gmail.com>
2022-09-21 19:11:25 -0300

    Add handle complete functionality

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -8,16 +8,37 @@ const TodoItem = ({ todoItem, setTodos, todos }) => {
     toast.success("Todo deleted successfully.");
   };
 
+  const handleComplete = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todoItem.id) {
+          return {
+            ...todoItem,
+            completed: !item.completed,
+          };
+        } else {
+          return { ...todoItem };
+        }
+      })
+    );
+  };
+
   return (
     <li className="todo-item text-neutral first:rounded-t-lg h-14 bg-secondary flex px-3 border-b-2 border-b-primary justify-between items-center">
       <div className="todo-wrapper flex gap-1 h-full flex-grow">
         <div className="check-container w-8 aspect-square flex">
           {todoItem.completed ? (
-            <div className="active check-circle relative border border-neutral h-[44%] aspect-square rounded-full cursor-pointer self-center">
+            <div
+              className="active check-circle relative border border-neutral h-[44%] aspect-square rounded-full cursor-pointer self-center"
+              onClick={handleComplete}
+            >
               <div className="check-icon active absolute w-4 h-3"></div>
             </div>
           ) : (
-            <div className="check-circle relative border border-neutral h-[44%] aspect-square rounded-full cursor-pointer self-center">
+            <div
+              className="check-circle relative border border-neutral h-[44%] aspect-square rounded-full cursor-pointer self-center"
+              onClick={handleComplete}
+            >
               <div className="absolute w-4 aspect-square"></div>
             </div>
           )}
